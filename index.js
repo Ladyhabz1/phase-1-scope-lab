4a1f0500eefc51afabd3c0148d8711c8e4d8041d
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2024-12-03 12:26:04 +0300

    Added functions to demonstrate scope and mutability

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Implicit global variable
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'initial customer';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new customer'; // Will throw an error
+}
